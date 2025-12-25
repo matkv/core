c18ed8b473eeda008e79f8dc80e73d24ac3b75e3
matkv <mail@matkv.dev>
2025-12-25 22:55:18 +0100

    started working on website sync functionality

--- internal/website/website.go
+++ internal/website/website.go
@@ -2,7 +2,10 @@ package website
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/matkv/core/internal/config"
 	"github.com/matkv/core/internal/obsidian"
 	"github.com/matkv/core/internal/types"
 )
@@ -39,7 +42,90 @@ func Scan() error {
 }
 
 func Sync() error {
-	fmt.Println("Syncing Obsidian content to Hugo site...")
-	// Placeholder for actual syncing logic
+	fmt.Println("Scanning the Obsidian vault...")
+
+	// TODO remove duplicate check in LoadContentFilesOfType
+	// check if obsidian vault path exists
+	vaultPath := config.C.Paths.ObsidianVault
+
+	if vaultPath == "" {
+		return fmt.Errorf("Obsidian vault path is not set in the configuration")
+	}
+
+	info, err := os.Stat(vaultPath)
+	if err != nil || !info.IsDir() {
+		return fmt.Errorf("Obsidian vault path does not exist or is not a directory: %s", vaultPath)
+	}
+
+	websitePath := config.C.Paths.Website
+
+	if websitePath == "" {
+		return fmt.Errorf("Website path is not set in the configuration")
+	}
+
+	var contentPath string = filepath.Join(websitePath, "content")
+
+	info, err = os.Stat(contentPath)
+	if err != nil || !info.IsDir() {
+		return fmt.Errorf("Content path does not exist or is not a directory: %s", websitePath)
+	}
+
+	fmt.Printf("Content path: %s\n", contentPath)
+
+	contentTypes := []types.ContentType{
+		types.BookReview{},
+		// types.MovieReview{},
+	}
+
+	if err := clearContentDirAndRecreate(contentPath); err != nil {
+		return fmt.Errorf("failed to clear content directory: %w", err)
+	}
+
+	setupFolderStructure(contentPath)
+	generateStandaloneFiles()
+
+	for _, ct := range contentTypes {
+		files, err := obsidian.LoadContentFilesOfType(ct)
+		if err != nil {
+			return fmt.Errorf("failed to load files for %s: %w", ct.TypeName(), err)
+		}
+
+		fmt.Printf("Syncing %d %s(s)...\n", len(files), ct.TypeName())
+		for _, f := range files {
+			fmt.Printf("Syncing file: %v\n", f)
+		}
+	}
+
+	fmt.Println("Do you want to sync the content?")
+
 	return nil
+}
+
+func clearContentDirAndRecreate(contentPath string) error {
+	fmt.Println("Clearing existing content in the website directory...")
+
+	if err := os.RemoveAll(contentPath); err != nil {
+		return err
+	}
+
+	return os.MkdirAll(contentPath, 0o755)
+}
+
+func setupFolderStructure(contentPath string) {
+	dirs := []string{
+		filepath.Join(contentPath, "garden"),
+		filepath.Join(contentPath, "library", "books"),
+		filepath.Join(contentPath, "log"),
+		filepath.Join(contentPath, "projects"),
+	}
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			fmt.Printf("Failed to create directory %s: %v\n", dir, err)
+		}
+	}
+}
+
+func generateStandaloneFiles() {
+	fmt.Println("Generating standalone files...")
+
 }
